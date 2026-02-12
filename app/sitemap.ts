be17bc5b85526dc9e571a4d16f6648663d862c85
said jadli <jadli.said00@gmail.com>
2026-02-12 19:58:36 +0100

    v0.9

---
+++ app/sitemap.ts
@@ -0,0 +1,40 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    const baseUrl = "https://saidjadli.com";
+
+    const routes = [
+        {
+            url: baseUrl,
+            lastModified: new Date(),
+            changeFrequency: "monthly" as const,
+            priority: 1,
+        },
+        {
+            url: `${baseUrl}/projects`,
+            lastModified: new Date(),
+            changeFrequency: "monthly" as const,
+            priority: 0.7,
+        },
+        {
+            url: `${baseUrl}/contact`,
+            lastModified: new Date(),
+            changeFrequency: "monthly" as const,
+            priority: 0.7,
+        },
+        {
+            url: `${baseUrl}/experience`,
+            lastModified: new Date(),
+            changeFrequency: "monthly" as const,
+            priority: 0.7,
+        },
+        {
+            url: `${baseUrl}/resume`,
+            lastModified: new Date(),
+            changeFrequency: "monthly" as const,
+            priority: 0.7,
+        },
+    ];
+
+    return routes;
+}
